3236facb0632d5aef62ba038238df164150af288
meilanikidd <meilani.kidd@gmail.com>
2025-07-31 17:26:54 -0400

    Update PlayerControllerHB.cs

--- Assets/health/PlayerControllerHB.cs
+++ Assets/health/PlayerControllerHB.cs
@@ -15,6 +15,7 @@ public class PlayerControllerHB : MonoBehaviour
     void Start()
     {
         currentHealth = maxHealth;
+        currentHealthNum = healthNumMax;
         UpdateHealthBar();
     }
 
